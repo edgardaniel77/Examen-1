4c8119ad3e1773c09a9ad4ff866c4018b371dc66
edgardaniel77 <edgardaguilera3@gmail.com>
2023-02-22 19:30:48 -0600

    segundo commit
    segundo cambio

--- examen 1/examen 1/Examen.Designer.cs
+++ examen 1/examen 1/Examen.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace examen_1
 {
-    partial class Form1
+    partial class Examen
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Examen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Examen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
